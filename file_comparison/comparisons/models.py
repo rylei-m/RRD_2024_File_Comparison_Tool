6355115d1fae321b6af0c4f4a72c37dc6de65572
Rylei Mindrum <rymindrum@gmail.com>
2024-07-10 09:34:05 -0600

    models starter

--- file_comparison/comparisons/models.py
+++ file_comparison/comparisons/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Document(models.Model):
+    file = models.FileField(upload_to='documents/')
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+
+class ComparisonResult(models.Model):
+    document_1 = models.ForeignKey(Document, related_name='document_1', on_delete=models.CASCADE)
+    document_2 = models.ForeignKey(Document, related_name='document_2', on_delete=models.CASCADE)
+    result = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
