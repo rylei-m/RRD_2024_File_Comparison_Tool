a97d28e2a336e394cb5ff5102bf12b651b9cf64f
Rylei Mindrum <rymindrum@gmail.com>
2024-07-10 09:37:02 -0600

    added djangorestframework

--- file_comparison/file_comparison/settings.py
+++ file_comparison/file_comparison/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'comparisons'
+    'comparisons',
+    'DRF',
 ]
 
 MIDDLEWARE = [
