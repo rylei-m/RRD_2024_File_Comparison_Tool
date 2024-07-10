89f66f54e406fa4e2abf16611cc621f1a07ec7a0
Rylei Mindrum <rymindrum@gmail.com>
2024-07-10 09:47:23 -0600

    views stuff but im not exactly sure how to do it yet/

--- file_comparison/comparisons/views.py
+++ file_comparison/comparisons/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import viewsets
+from rest_framework.response import Response
+from .models import Document, ComparisonResult
+from .serializers import DocumentSerializer, ComparisonResultSerializer
+
+
+class DocumentViewSet(viewsets.ModelViewSet):
+    queryset = Document.objects.all()
+    serializer_class = DocumentSerializer
+
+
+class ComparisonViewSet(viewsets.ViewSet):
+    def create(self, request):
+        #logic here later
+        pass
